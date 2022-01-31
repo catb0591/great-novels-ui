93327f38ad981011bd1fac0865b355d4124364e4
Cat <catb0591@gmail.com>
2022-01-30 21:37:42 -0500

    added scripts for concurrently

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { getAllNovels, getNovelByIdOrTitle } = require('./controllers/novels')
 const path = require('path')
 
 const app = express()
+
 app.use(cors())
 app.use(express.static('client/build'))
 
@@ -18,7 +19,7 @@ app.get('/api/genres/:id', getGenreById)
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
 
-app.all('*', (request, response) => response.sendFile(path.resolve(__dirname,'client/build', 'index.html')))
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname, 'client/build', 'index.html')))
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
