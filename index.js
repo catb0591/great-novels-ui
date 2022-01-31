9c13b92d9c85c8d3e24267a52c0e406088b0d68a
Cat <catb0591@gmail.com>
2022-01-30 21:32:19 -0500

    updated the index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const cors = require('cors')
 const { getAllAuthors, getAuthorByIdOrName } = require('./controllers/authors')
 const { getAllGenres, getGenreById } = require('./controllers/genres')
 const { getAllNovels, getNovelByIdOrTitle } = require('./controllers/novels')
+const path = require('path')
 
 const app = express()
 app.use(cors())
+app.use(express.static('client/build'))
 
 app.get('/api/authors', getAllAuthors)
 app.get('/api/authors/:identifier', getAuthorByIdOrName)
@@ -15,6 +17,8 @@ app.get('/api/genres/:id', getGenreById)
 
 app.get('/api/novels', getAllNovels)
 app.get('/api/novels/:identifier', getNovelByIdOrTitle)
+
+app.all('*', (request, response) => response.sendFile(path.resolve(__dirname,'client/build', 'index.html')))
 
 app.listen(1337, () => {
   console.log('Listening on port 1337...') // eslint-disable-line no-console
